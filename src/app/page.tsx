b41e28a98ccc18fd07f89c52d70ab87f689c4627
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-01-21 17:52:05 -0300

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
-import Head from "next/head"
-import styles from '../styles/home.module.css'
+import Head from "next/head";
+import styles from "../styles/home.module.css";
+import Image from "next/image";
 
-export default function Home(){
-    return(
-        <div className={styles.container}>
-            <Head>
-                <title>Next.js App</title>
-            </Head>
+import heroImg from "../../public/hero.png";
 
-            <h1>Meu projeto</h1>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next.js App</title>
+      </Head>
+
+      <div className={styles.logoContent}>
+        <Image className={styles.hero} alt="Logo Tarefas+" src={heroImg} />
+      </div>
+      <h1 className={styles.title}>
+        Sistema feito para Organizar seu dia dia
+        <br />
+        <span>Tarefa + </span>
+      </h1>
+    </div>
+  );
 }
